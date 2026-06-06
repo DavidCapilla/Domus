c119038d707142e94ade7ddbf48840fa7e035e39
David Capilla Guilarte <david.capilla.guilarte@ast.cam.ac.uk>
2026-06-07 00:12:57 +0200

    #2 Add chore repository

--- src/main/kotlin/com/domus/core/chore/ChoreRepository.kt
+++ src/main/kotlin/com/domus/core/chore/ChoreRepository.kt
@@ -1,10 +1,5 @@
 package com.domus.core.chore
 
-import java.util.UUID
-
 interface ChoreRepository {
     fun findAll(): List<Chore>
-    fun findById(id: UUID): Chore?
-    fun save(chore: Chore): Chore
-    fun deleteById(id: UUID)
 }
