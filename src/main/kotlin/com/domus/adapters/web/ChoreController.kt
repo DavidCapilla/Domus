6e25c28f210e6f6414a50b0b7a1392b76290ba85
David Capilla Guilarte <david.capilla.guilarte@ast.cam.ac.uk>
2026-06-06 23:41:18 +0200

    #2 Add Chore controller with mock response

--- src/main/kotlin/com/domus/adapters/web/ChoreController.kt
+++ src/main/kotlin/com/domus/adapters/web/ChoreController.kt
@@ -1,15 +1,16 @@
-package com.domus.controller
+package com.domus.adapters.web
 
+import com.domus.core.chore.Chore
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class HelloController {
+class ChoreController {
 
-    @GetMapping("/hello")
-    fun hello(): Map<String, String> {
-        return mapOf("message" to "Hello from Domus!")
+    @GetMapping("/chores")
+    fun getChores(): List<Chore> {
+        return listOf(Chore("Placeholder chore"))
     }
 }
