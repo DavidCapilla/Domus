066f00141ab48efdb2cae526b4e17e2c39491a9e
David Capilla Guilarte <david.capilla.guilarte@ast.cam.ac.uk>
2026-06-07 02:12:26 +0200

    #6 Create delete chore usecase

---
+++ src/main/kotlin/com/domus/application/chore/DeleteChoreUseCase.kt
@@ -0,0 +1,16 @@
+package com.domus.application.chore
+
+import com.domus.core.chore.Chore
+import com.domus.core.chore.ChoreNotFoundException
+import com.domus.core.chore.ChoreRepository
+import org.springframework.stereotype.Service
+
+@Service
+class DeleteChoreUseCase(val choreRepository: ChoreRepository) {
+
+     fun deleteChore(choreName: String) {
+         if (!choreRepository.delete(Chore(choreName))) {
+             throw ChoreNotFoundException(choreName)
+         }
+     }
+}
