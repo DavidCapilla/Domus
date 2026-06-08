4cbe65c5f8f8e03ca6d0ac839325d40db15675a1
David Capilla Guilarte <davidcapillag@gmail.com>
2026-06-08 21:00:48 +0200

    #21 Add update to chore repository

--- src/main/kotlin/com/domus/adapters/persistence/InMemoryChoreRepository.kt
+++ src/main/kotlin/com/domus/adapters/persistence/InMemoryChoreRepository.kt
@@ -3,7 +3,6 @@ package com.domus.adapters.persistence
 import com.domus.core.chore.Chore
 import com.domus.core.chore.ChoreRepository
 import org.springframework.stereotype.Repository
-import java.util.UUID
 
 @Repository
 class InMemoryChoreRepository() : ChoreRepository {
@@ -12,9 +11,18 @@ class InMemoryChoreRepository() : ChoreRepository {
 
     override fun findAll() = chores.toList()
 
+    override fun findByName(name: String) = chores.find { it.name == name }
+
     override fun save(chore: Chore): Boolean {
         if (chores.any { it.name == chore.name }) return false
         return chores.add(chore)
+    }
+
+    override fun update(currentName: String, chore: Chore): Boolean {
+        val existing = chores.find { it.name == currentName } ?: return false
+        chores.remove(existing)
+        chores.add(chore)
+        return true
     }
 
     override fun delete(choreName: String) = chores.removeIf { it.name == choreName }
--- src/main/kotlin/com/domus/core/chore/ChoreRepository.kt
+++ src/main/kotlin/com/domus/core/chore/ChoreRepository.kt
@@ -2,6 +2,8 @@ package com.domus.core.chore
 
 interface ChoreRepository {
     fun findAll(): List<Chore>
+    fun findByName(name: String): Chore?
     fun save(chore: Chore): Boolean
+    fun update(currentName: String, chore: Chore): Boolean
     fun delete(choreName: String): Boolean
 }
