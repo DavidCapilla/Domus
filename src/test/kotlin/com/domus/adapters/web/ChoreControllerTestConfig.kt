ecd7f1901906e0a1b5628030a59b701027058718
David Capilla Guilarte <davidcapillag@gmail.com>
2026-06-08 21:01:11 +0200

    #21 Update the mock

--- src/test/kotlin/com/domus/adapters/web/ChoreControllerTestConfig.kt
+++ src/test/kotlin/com/domus/adapters/web/ChoreControllerTestConfig.kt
@@ -45,10 +45,19 @@ class FakeChoreRepository : ChoreRepository {
 
     override fun findAll(): List<Chore> = chores.toList()
 
+    override fun findByName(name: String) = chores.find { it.name == name }
+
     override fun save(chore: Chore): Boolean {
         val alreadyExists = chores.any { it.name == chore.name }
         if (!alreadyExists) chores.add(chore)
         return !alreadyExists
+    }
+
+    override fun update(currentName: String, chore: Chore): Boolean {
+        val existing = chores.find { it.name == currentName } ?: return false
+        chores.remove(existing)
+        chores.add(chore)
+        return true
     }
 
     override fun delete(choreName: String): Boolean {
