a2cd63c21ddfedfdbc42bc41eae04b3de5ee265c
David Capilla Guilarte <davidcapillag@gmail.com>
2026-06-09 23:05:11 +0200

    #25 Extract DashboardDto

--- src/main/kotlin/com/domus/adapters/web/dto/ChoreDto.kt
+++ src/main/kotlin/com/domus/adapters/web/dto/ChoreDto.kt
@@ -2,7 +2,6 @@ package com.domus.adapters.web.dto
 
 import com.domus.core.chore.Chore
 import com.domus.core.chore.Schedule
-import com.domus.core.dashboard.Dashboard
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import java.time.LocalDate
@@ -43,20 +42,6 @@ data class ChoreResponse(
             name = chore.name,
             dueDate = chore.dueDate,
             schedule = chore.schedule.toDto(),
-        )
-    }
-}
-
-data class DashboardResponse(
-    val overdue: List<ChoreResponse>,
-    val dueToday: List<ChoreResponse>,
-    val upcoming: List<ChoreResponse>,
-) {
-    companion object {
-        fun fromDomain(dashboard: Dashboard) = DashboardResponse(
-            overdue = dashboard.overdue.map(ChoreResponse::fromDomain),
-            dueToday = dashboard.dueToday.map(ChoreResponse::fromDomain),
-            upcoming = dashboard.upcoming.map(ChoreResponse::fromDomain),
         )
     }
 }
