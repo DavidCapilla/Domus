0e349957006a63de3a04f154e448950ded8a00c7
David Capilla Guilarte <davidcapillag@gmail.com>
2026-06-20 06:42:52 +0200

    #54 Add event logging

--- src/main/kotlin/com/domus/chores/application/DeleteChoreUseCase.kt
+++ src/main/kotlin/com/domus/chores/application/DeleteChoreUseCase.kt
@@ -2,14 +2,21 @@ package com.domus.chores.application
 
 import com.domus.chores.core.ChoreNotFoundException
 import com.domus.chores.core.ChoreRepository
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class DeleteChoreUseCase(val choreRepository: ChoreRepository) {
 
+    companion object {
+        private val log = LoggerFactory.getLogger(DeleteChoreUseCase::class.java)
+    }
+
     fun deleteChore(choreName: String) {
         if (!choreRepository.delete(choreName)) {
+            log.warn("Chore not found for deletion [name={}]", choreName)
             throw ChoreNotFoundException(choreName)
         }
+        log.info("Chore deleted [name={}]", choreName)
     }
 }
