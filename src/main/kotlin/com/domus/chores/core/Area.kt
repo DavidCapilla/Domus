20a2dc706b5eb83071ab9d862fd5a45a195c9e9f
David Capilla Guilarte <davidcapillag@gmail.com>
2026-07-05 21:44:10 +0200

    #89 Remove unnecessary display name

--- src/main/kotlin/com/domus/chores/core/Area.kt
+++ src/main/kotlin/com/domus/chores/core/Area.kt
@@ -1,11 +1,11 @@
 package com.domus.chores.core
 
-enum class Area(val displayName: String) {
-    KITCHEN("Kitchen"),
-    BATHROOM("Bathroom"),
-    EXTERIOR("Exterior"),
-    LIVING_ROOM("Living room"),
-    BEDROOM("Bedroom"),
-    ENTIRE_HOUSE("Entire house"),
-    NONE("None"),
+enum class Area {
+    KITCHEN,
+    BATHROOM,
+    EXTERIOR,
+    LIVING_ROOM,
+    BEDROOM,
+    ENTIRE_HOUSE,
+    NONE,
 }
